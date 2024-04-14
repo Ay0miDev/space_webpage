e30b7bcb71918253cfff8283d0887879c4b92ecd
ayomide <sulaimanayomide005@gmail.com>
2024-04-14 22:32:06 +0100

    JS section

---
+++ starter-code/destination-mars.js
@@ -0,0 +1,6 @@
+// Destination Main Container 
+const section = document.createElement("section");
+section.classList.add('marsDetails');
+
+const destinationAndImage = document.createElement('div')
+destinationAndImage.classList.add('destinationAndimage')
