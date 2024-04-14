fbe804846394e380f10d84cc22cc40e3cc3c078b
ayomide <sulaimanayomide005@gmail.com>
2024-04-14 23:13:48 +0100

    10%  Mars Info

--- starter-code/destination-mars.js
+++ starter-code/destination-mars.js
@@ -7,22 +7,35 @@ section.classList.add('marsDetails');
 const destinationAndImage = document.createElement('div');
 destinationAndImage.classList.add('destinationAndimage');
 const destination = document.createElement('h2');
-destination.classList.add('destination')
+destination.classList.add('destination');
 const zeroone = document.createElement('span');
 zeroone.id = 'zero&one'
+const zerooneText = document.createTextNode('01');
 
 // Appending the first div
+zeroone.appendChild(zerooneText);
+
+const destinationText = document.createTextNode(`pick your destination`)
+destination.appendChild(destinationText);
+
 destination.appendChild(zeroone);
 destinationAndImage.appendChild(destination);
+console.log(destinationAndImage);
 
 const marsInfo = document.createElement('div');
 marsInfo.classList.add('marsInfo');
-const moonImg = document.createElement('img');
 
 // The Mars Image Container
+const moonImg = document.createElement('img');
 moonImg.classList.add('moonImg');
 moonImg.src = './assets/destination/image-mars.webp'
 moonImg.setAttribute('alt', 'THE MARS');
+
+// A new div container
+const planet = document.createElement('div');
+planet.classList.add('planet');
+
+
 
 
 
