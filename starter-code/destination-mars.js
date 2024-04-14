bd9f578621dd0b9e6a165b3b7548f3c65f6d352e
ayomide <sulaimanayomide005@gmail.com>
2024-04-14 22:44:47 +0100

    appended the first div child

--- starter-code/destination-mars.js
+++ starter-code/destination-mars.js
@@ -1,6 +1,18 @@
 // Destination Main Container 
+
+// THE SECTION CONTAINER
 const section = document.createElement("section");
 section.classList.add('marsDetails');
 
-const destinationAndImage = document.createElement('div')
-destinationAndImage.classList.add('destinationAndimage')
+const destinationAndImage = document.createElement('div');
+destinationAndImage.classList.add('destinationAndimage');
+const destination = document.createElement('h2');
+destination.classList.add('destination')
+const zeroone = document.createElement('span');
+zeroone.id = 'zero&one'
+
+// Appending the first div
+destination.appendChild(zeroone)
+destinationAndImage.appendChild(destination)
+
+
