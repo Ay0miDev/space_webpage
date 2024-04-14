2a19a919d28f585287b1c6f309957feb95c654cd
ayomide <sulaimanayomide005@gmail.com>
2024-04-14 23:00:01 +0100

    THE MOON

--- starter-code/destination-mars.js
+++ starter-code/destination-mars.js
@@ -12,7 +12,18 @@ const zeroone = document.createElement('span');
 zeroone.id = 'zero&one'
 
 // Appending the first div
-destination.appendChild(zeroone)
-destinationAndImage.appendChild(destination)
+destination.appendChild(zeroone);
+destinationAndImage.appendChild(destination);
+
+const marsInfo = document.createElement('div');
+marsInfo.classList.add('marsInfo');
+const moonImg = document.createElement('img');
+
+// The Mars Image Container
+moonImg.classList.add('moonImg');
+moonImg.src = './assets/destination/image-mars.webp'
+moonImg.setAttribute('alt', 'THE MARS');
+
+
 
 
