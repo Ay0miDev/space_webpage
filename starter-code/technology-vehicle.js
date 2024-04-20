fb762879dedc9abc0403d28188be63b79970781e
ayomide <sulaimanayomide005@gmail.com>
2024-04-20 17:11:32 +0100

    30% technology page

--- starter-code/technology-vehicle.js
+++ starter-code/technology-vehicle.js
@@ -3,6 +3,14 @@ const numOne = document.getElementsByClassName('numOrder')[0];
 const numTwo = document.getElementsByClassName('numOrder')[1];
 const numThree = document.getElementsByClassName('numOrder')[2];
 
+// Locating the Container of the Description text
+const terminology = document.getElementById('terminology');
+// Creating a new text in javascript
+const space_port = document.createElement(h1);
+space_port.setAttribute('id', 'launchVehicle');
+space_port = document.innerHTML = `Spaceport`;
+const space_port_Info = document.createElement
+
 // numOne.addEventListener('click', function changeImg() {
 if(eventImg.setAttribute('alt', 'LAUNCH VEHICLE')) {
     eventImg.setAttribute('src', './assets/technology/image-launch-vehicle-portrait.jpg')
