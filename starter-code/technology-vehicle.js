7f3618bc3287a81bcdb257fcf7aa137f2939514f
ayomide <sulaimanayomide005@gmail.com>
2024-04-20 16:58:07 +0100

    javascript function

---
+++ starter-code/technology-vehicle.js
@@ -0,0 +1,18 @@
+const eventImg = document.getElementById('launchVehicleImg');
+const numOne = document.getElementsByClassName('numOrder')[0];
+const numTwo = document.getElementsByClassName('numOrder')[1];
+const numThree = document.getElementsByClassName('numOrder')[2];
+
+// numOne.addEventListener('click', function changeImg() {
+if(eventImg.setAttribute('alt', 'LAUNCH VEHICLE')) {
+    eventImg.setAttribute('src', './assets/technology/image-launch-vehicle-portrait.jpg')
+}else if(eventImg.alt != '') {
+
+}
+
+
+if (condition) {
+    
+} else {
+    
+}
