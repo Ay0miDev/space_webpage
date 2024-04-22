965f3e86c03c5eacc9496440503dcef3f7192a9d
ayomide <sulaimanayomide005@gmail.com>
2024-04-22 21:08:15 +0100

    100% technology page

--- starter-code/technology-vehicle.js
+++ starter-code/technology-vehicle.js
@@ -15,17 +15,11 @@ spacePostTerminology.innerHTML = 'The terminology...';
 
 const space_port = document.createElement('h1');
 space_port.setAttribute('id', 'spacePort');
-space_port.style.fontFamily = 'none';
-space_port.style.fontWeight = '300';
-space_port.style.fontSize = '60px';
 space_port.innerHTML = `Spaceport`;
 
 const space_port_Info = document.createElement('p');
 space_port_Info.setAttribute('id', 'spacePortInfo');
 space_port_Info.style.textTransform = 'capitalize';
-space_port_Info.style.lineHeight = '30px';
-space_port_Info.style.letterSpacing = '2px';
-space_port_Info.style.color = 'rgba(255, 255, 255, 0.43';
 const space_port_Info_Text = document.createTextNode(`A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, 
 by analogy to the seaport for ships or airport for aircraft. Based in the 
 famous Cape Canaveral, our spaceport is ideally situated to take advantage 
@@ -54,17 +48,11 @@ capsuleTerminology.innerHTML = 'The terminology...';
 
 const capsule = document.createElement('h1');
 capsule.setAttribute('id', 'capsule');
-capsule.style.fontFamily = 'none';
-capsule.style.fontWeight = '300';
-capsule.style.fontSize = '60px';
 capsule.innerHTML = `Space capsule`;
 
 const capsule_Info = document.createElement('p');
 capsule_Info.setAttribute('id', 'capsuleInfo');
 capsule_Info.style.textTransform = 'capitalize';
-capsule_Info.style.lineHeight = '30px';
-capsule_Info.style.letterSpacing = '2px';
-capsule_Info.style.color = 'rgba(255, 255, 255, 0.43';
 const capsule_Info_Text = document.createTextNode(`A space capsule is an often-crewed spacecraft that uses a blunt-body reentry 
 capsule to reenter the Earth's atmosphere without wings. Our capsule is where 
 you'll spend your time during the flight. It includes a space gym, cinema, 
@@ -128,59 +116,3 @@ document.addEventListener('click', function() {
         numThree.style.color = 'black'
     }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const launchVehicle = document.getElementById('launchVehicle');
-
-
-
-// if(numOne.addEventListener('click', function(){
-
-// })) {
-//     tech_Info.style.display = 'none'
-//     tech_Info_Capsule.style.display = 'none'
-// } else if(numTwo.addEventListener('click', function(){
-
-// })) {
-//     tech_Info.style.display = 'block'
-//     tech_Info_Capsule.style.display = 'none'
-// } else{
-//     tech_Info.style.display = 'none'
-//     tech_Info_Capsule.style.display = 'block'
-// }
-
-
-
-
-
-
-// numOne.addEventListener('click', function changeImg() {
-if(eventImg.setAttribute('alt', 'LAUNCH VEHICLE')) {
-    eventImg.setAttribute('src', './assets/technology/image-launch-vehicle-portrait.jpg')
-}else if(eventImg.alt != '') {
-
-}
-
-
-if (condition) {
-    
-} else {
-    
-}
