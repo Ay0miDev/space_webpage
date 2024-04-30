cc42c1dedcff786f19799d7a05dd954e2c3939de
ayomide <sulaimanayomide005@gmail.com>
2024-04-30 23:44:01 +0100

    stuck

--- starter-code/destinationPageEvent.js
+++ starter-code/destinationPageEvent.js
@@ -2,6 +2,8 @@
 
 function showDestinationInfo (optionDestination) {
     marsPlanet.style.display = optionDestination === 0 ? 'flex' : 'none'
-    
+    planetMoon.style.display = optionDestination === 1 ? 'flex' : 'none'
+    planetEuropa.style.display = optionDestination === 2 ? 'flex' : 'none'
+    planetTitan.style.display = optionDestination === 3 ? 'flex' : 'none'
 
 }
