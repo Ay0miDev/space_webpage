bbfac6bdeda92919545060d1d86d9dd3ff7bbfbb
ayomide <sulaimanayomide005@gmail.com>
2024-05-09 15:57:53 +0100

    40% dest page

--- starter-code/destination-europa.js
+++ starter-code/destination-europa.js
@@ -1,47 +1,9 @@
 
 // CREATING THE MOON SEGMENT
 
-// Creating a new div element, nesting all elements
-const planetEuropa = document.createElement('div');
-planetEuropa.classList.add('planetEuropa');
-
-const theEuropaandPlanets = document.createElement('ul');
-theEuropaandPlanets.classList.add('theEuropaandPlanets');
-
-const europaList01 = document.createElement('li');
-const europaSolarMars = document.createElement('a');
-europaSolarMars.setAttribute('href', "")
-europaSolarMars.classList.add('solarSystem');
-europaSolarMars.innerHTML = 'mars'
-
-const europaList02 = document.createElement('li');
-const europaSolarMoon = document.createElement('a');
-europaSolarMoon.setAttribute('href', "")
-europaSolarMoon.classList.add('solarSystem');
-europaSolarMoon.innerHTML = 'europa'
-
-const europaList03 = document.createElement('li');
-const europaSolarEuropa = document.createElement('a');
-europaSolarEuropa.setAttribute('href', "")
-europaSolarEuropa.classList.add('solarSystem');
-europaSolarEuropa.innerHTML = 'europa'
-
-const europaList04 = document.createElement('li');
-const europaSolarTitans = document.createElement('a');
-europaSolarTitans.setAttribute('href', "")
-europaSolarTitans.classList.add('solarSystem');
-europaSolarTitans.innerHTML = 'titans'
-
-
-europaList01.appendChild(europaSolarMars);
-europaList02.appendChild(europaSolarMoon);
-europaList03.appendChild(europaSolarEuropa);
-europaList04.appendChild(europaSolarTitans);
-
-theEuropaandPlanets.appendChild(europaList01);
-theEuropaandPlanets.appendChild(europaList02);
-theEuropaandPlanets.appendChild(europaList03);
-theEuropaandPlanets.appendChild(europaList04);
+// Creating a new div element to replace theMoonDetails
+const theEuropaDetail = document.createElement('div');
+theEuropaDetail.setAttribute('id', 'theEuropaDetail');
 
 
 // Creating a h1 element
@@ -84,27 +46,54 @@ europaTimeDetail.appendChild(europaDuration);
 europa_timeAndDistance.appendChild(europaDistanceDetail);
 europa_timeAndDistance.appendChild(europaTimeDetail);
 
-planetEuropa.appendChild(theEuropaandPlanets);
-planetEuropa.appendChild(europa);
-planetEuropa.appendChild(europaInformation);
-planetEuropa.appendChild(europa_timeAndDistance);
 
-marsInfo.appendChild(planetEuropa)
+theEuropaDetail.appendChild(europa);
+theEuropaDetail.appendChild(europaInformation);
+theEuropaDetail.appendChild(europa_timeAndDistance);
 
-// const marsPlanet = document.querySelector('.planet')
-// marsPlanet.style.display = 'flex'
-// marsPlanet.style.flexDirection = 'column'
-// console.log(marsPlanet);
+thePlanet.appendChild(theEuropaDetail)
+theEuropaDetail.style.display = 'block'
 
-// // The Moon Page
-// planetMoon.style.display = 'none'
+// iterating the main MoonDetails
+theMoonDetail.style.display = 'none'
 
-// // The Europa Page
-// planetEuropa.style.display = 'flex'
-// planetEuropa.style.flexDirection = 'column'
-// planetEuropa.style.display = 'none'
-// console.log(planetEuropa);
 
-// marsPlanet.style.display = 'none'
-planetMoon.style.display = 'none'
-planetEuropa.style.display = 'none'
+
+// const theEuropaandPlanets = document.createElement('ul');
+// theEuropaandPlanets.classList.add('theEuropaandPlanets');
+
+// const europaList01 = document.createElement('li');
+// const europaSolarMars = document.createElement('a');
+// europaSolarMars.setAttribute('href', "")
+// europaSolarMars.classList.add('solarSystem');
+// europaSolarMars.innerHTML = 'mars'
+
+// const europaList02 = document.createElement('li');
+// const europaSolarMoon = document.createElement('a');
+// europaSolarMoon.setAttribute('href', "")
+// europaSolarMoon.classList.add('solarSystem');
+// europaSolarMoon.innerHTML = 'europa'
+
+// const europaList03 = document.createElement('li');
+// const europaSolarEuropa = document.createElement('a');
+// europaSolarEuropa.setAttribute('href', "")
+// europaSolarEuropa.classList.add('solarSystem');
+// europaSolarEuropa.innerHTML = 'europa'
+
+// const europaList04 = document.createElement('li');
+// const europaSolarTitans = document.createElement('a');
+// europaSolarTitans.setAttribute('href', "")
+// europaSolarTitans.classList.add('solarSystem');
+// europaSolarTitans.innerHTML = 'titans'
+
+
+// europaList01.appendChild(europaSolarMars);
+// europaList02.appendChild(europaSolarMoon);
+// europaList03.appendChild(europaSolarEuropa);
+// europaList04.appendChild(europaSolarTitans);
+
+// theEuropaandPlanets.appendChild(europaList01);
+// theEuropaandPlanets.appendChild(europaList02);
+// theEuropaandPlanets.appendChild(europaList03);
+// theEuropaandPlanets.appendChild(europaList04);
+
